c8c3d72516f2c374bcea10d02ea1c8bbd84d6aa7
AminTurmudiYusup <aminturmudzi@gmail.com>
2024-11-06 12:39:31 +0700

    Your commit message here

--- 01.IO_flush/first_io.c
+++ 01.IO_flush/first_io.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
-    puts("STDOUT");
-    //fflush(stdout);
-    fputs("STDER\n", stderr);
-    //fflush(stderr);
+int main() {
+    FILE *file = fopen("output.txt", "w");
+    if (file == NULL) {
+        perror("Failed to open file");
+        return 1;
+    }
+
+    
+    puts("STDOUT\n");             
+    fprintf(file, "STDOUT\n");    
+
+  
+    fputs("STDER\n", stderr);    
+    fprintf(file, "STDER\n");
+
+    fclose(file);
     return 0;
 }
