c70afa332ec17828f964d2397e3f9f6acffd3745
AminTurmudiYusup <aminturmudzi@gmail.com>
2024-11-06 17:09:28 +0700

    modify program

---
+++ 01.IO_flush/bash_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Name of the C file and output executable
+C_FILE="flush_io.c"
+OUTPUT_FILE="program"
+
+# Compile the program
+echo "Compiling $C_FILE..."
+gcc "$C_FILE" -o "$OUTPUT_FILE"
+if [ $? -ne 0 ]; then
+    echo "Compilation failed."
+    exit 1
+fi
+
+echo "Compilation successful."
+
+# Run the program with parameter 'a'
+echo -e "\nRunning with parameter 'a':"
+./"$OUTPUT_FILE" a
+
+# Run the program with parameter 'b'
+echo -e "\nRunning with parameter 'b':"
+./"$OUTPUT_FILE" b
+
+# Run the program with parameter 'c'
+echo -e "\nRunning with parameter 'c':"
+./"$OUTPUT_FILE" c
--- 01.IO_flush/first_io.c
+++ 01.IO_flush/first_io.c
@@ -1,20 +1,65 @@
 #include <stdio.h>
 
-int main() {
-    FILE *file = fopen("output.txt", "w");
+void printFflush(){
+
+    puts("STDOUT With fflush");  
+    fflush(stdout);
+
+    fputs("STDER With fflush", stderr); 
+    fflush(stderr);
+}
+
+
+void printN(){
+
+    puts("STDOUT With N \n");  
+
+    fputs("STDER With N \n", stderr); 
+}
+
+int printNAndWriteFile(){
+
+   FILE *file = fopen("output.txt", "w");
     if (file == NULL) {
         perror("Failed to open file");
         return 1;
     }
 
     
-    puts("STDOUT\n");             
-    fprintf(file, "STDOUT\n");    
+    puts("STDOUT With N \n");             
+    fprintf(file, "STDOUT With N \n");    
 
   
-    fputs("STDER\n", stderr);    
-    fprintf(file, "STDER\n");
+    fputs("STDER With N \n", stderr);    
+    fprintf(file, "STDER With N \n");
 
     fclose(file);
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <option>\nOptions:\n a - printFflush\n b - printN\n c - printNAndWriteFile\n", argv[0]);
+        return 1;
+    }
+
+    char option = argv[1][0];  // Get the first character of the argument
+
+    switch (option) {
+        case 'a':
+            printFflush();
+            break;
+        case 'b':
+            printN();
+            break;
+        case 'c':
+            printNAndWriteFile();
+            break;
+        default:
+            fprintf(stderr, "Unknown option: %c\n", option);
+            return 1;
+    }
+
     return 0;
 }
